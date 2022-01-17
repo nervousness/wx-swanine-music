104e60e64f4971ca2d9c42c7d4ce9026cafa3948
swanine <haohao1500@foxmail.com>
2022-01-17 21:55:30 +0800

    ✨ feat: home music page swiper config

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,66 +1,52 @@
 // pages/home-music/index.js
+// import Toast from '@vant/weapp/toast/toast'
+import {
+  getBanner
+} from "../../service/api_music"
+// import queryRect from '../../utils/query-rect'
+// import throttle from '../../utils/throttle'
+
+// const throttleQueryRect = throttle(queryRect)
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // swiperHeight: 0,
+    value: '',
+    banners: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getPageData()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 网络请求
+  getPageData: function () {
+    getBanner().then(res => {
+      this.setData({
+        banners: res.banners
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onClick() {
+    wx.navigateTo({
+      url: '/pages/detail-search/index'
+    })
   }
+
+  // handleSwiperImageLoaded: function() {
+  //   // 获取图片高度动态添加swiper container
+  //   throttleQueryRect('.image').then(res => {
+  //     console.log('ss')
+  //     const rect = res[0]
+  //     this.setData({ swiperHeight: rect.height })
+  //   })
+  // }
 })
