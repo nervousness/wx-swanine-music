1508e126e5e050be84376f5cd0b205b32f2943f1
swanine <haohao1500@foxmail.com>
2022-01-17 21:53:51 +0800

    💠 feat(api): video api

--- service/api_video.js
+++ service/api_video.js
@@ -6,3 +6,33 @@ export function getTopMV(offset, limit = 10) {
     limit
   })
 }
+
+/**
+ * 请求MV URL
+ * @param { number } id 
+ */
+export function getMVURL(id) {
+  return swanRequest.get("/mv/url", {
+    id
+  })
+}
+
+/**
+ * 请求MV详情
+ * @param { number } mvid 
+ */
+export function getMVDetail(mvid) {
+  return swanRequest.get("/mv/detail", {
+    mvid
+  })
+}
+
+/**
+ * 请求MV详情相关联视频
+ * @param { number } id 
+ */
+export function getRelatedVideo(id) {
+  return swanRequest.get("/related/allvideo", {
+    id
+  })
+}
